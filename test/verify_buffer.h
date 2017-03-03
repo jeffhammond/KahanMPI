d156b98ad0921d587894d9023eea0e7652aa0794
Jeff Hammond <jeff.science@gmail.com>
2017-03-03 11:14:43 -0800

    remove MPI/assert, add float

--- test/verify_buffer.h
+++ test/verify_buffer.h
@@ -2,18 +2,12 @@
 #define VERIFY_BUFFER_H
 
 #include <stdio.h>
-#include <stdint.h>
-#include <assert.h>
 #include <math.h>
-
-#include <mpi.h>
 
 /* This is the generic buffer verification for when char is the
  * type and we use memset for assignment. */
-static size_t verify_buffer(char *buf, MPI_Count count, int expected_value)
+static size_t verify_buffer(char *buf, size_t count, int expected_value)
 {
-    assert(count<SIZE_MAX);
-
     size_t errors = 0;
     for (size_t i = 0; i < (size_t)count; i++) {
         errors += (buf[i] != (unsigned char)expected_value);
@@ -21,23 +15,36 @@ static size_t verify_buffer(char *buf, MPI_Count count, int expected_value)
     return errors;
 }
 
-static size_t verify_doubles(double *buf, MPI_Count count, double expected_value)
+static size_t verify_doubles(double *buf, size_t count, double expected_value)
 {
-    assert(count<SIZE_MAX);
-
     size_t errors = 0;
     for (size_t i = 0; i < (size_t)count; i++) {
-        double absdiff = fabs(buf[i] - expected_value);
+        const double absdiff = fabs(buf[i] - expected_value);
         if (absdiff>1.e-4) errors++;
     }
     return errors;
 }
 
-static void set_doubles(double *buf, MPI_Count count, double value)
+static void set_doubles(double *buf, size_t count, double value)
 {
-    assert(count<SIZE_MAX);
-
     for (size_t i = 0; i < (size_t)count; i++) {
+        buf[i] = value;
+    }
+}
+
+static size_t verify_floats(float *buf, size_t count, float expected_value)
+{
+    size_t errors = 0;
+    for (size_t i = 0; i < count; i++) {
+        const float absdiff = fabs(buf[i] - expected_value);
+        if (absdiff>1.e-4) errors++;
+    }
+    return errors;
+}
+
+static void set_floats(float *buf, size_t count, float value)
+{
+    for (size_t i = 0; i < count; ++i) {
         buf[i] = value;
     }
 }
